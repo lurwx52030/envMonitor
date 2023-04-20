356eaa7531cdd98e1ea944f876d0c3278c9d31e5
sobol <nppstcc1001050311@gmail.com>
2023-04-21 00:00:53 +0800

    no message

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import RPi.GPIO as GPIO
 from PIL import ImageFont
-import subprocess
 import threading
 import time
 from ili9341_Adafruit import ili9341_Adafruit
@@ -82,41 +81,9 @@ def micTask():
         time.sleep(0.5)
 
 
-def piSysInfo():
-    global font
-
-    # 把畫面清空
-    screen.draw.rectangle((0, 0, 340, 240), fill="#000000")
-
-    cmd = "hostname -I | cut -d' ' -f1"
-    IP = "IP: " + subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU負載: %.2f%%\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "free -m | awk 'NR==2{printf \"記憶體: %s/%s MB  %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = 'df -h | awk \'$NF=="/"{printf "硬碟: %d/%d GB  %s", $3,$2,$5}\''
-    Disk = subprocess.check_output(cmd, shell=True).decode("utf-8")
-    cmd = "cat /sys/class/thermal/thermal_zone0/temp |  awk '{printf \"CPU溫度: %.1f C\", $(NF-0) / 1000}'"
-    Temp = subprocess.check_output(cmd, shell=True).decode("utf-8")
-
-    x, y = 0, -2
-
-    screen.draw.text((x, y), IP, font=font, fill="#FFFFFF")
-    y += font.getsize(IP)[1]
-    screen.draw.text((x, y), CPU, font=font, fill="#FFFF00")
-    y += font.getsize(CPU)[1]
-    screen.draw.text((x, y), MemUsage, font=font, fill="#00FF00")
-    y += font.getsize(MemUsage)[1]
-    screen.draw.text((x, y), Disk, font=font, fill="#0000FF")
-    y += font.getsize(Disk)[1]
-    screen.draw.text((x, y), Temp, font=font, fill="#FF00FF")
-
-    screen.disp.image(screen.image)
-
-
 def envMonitor():
     global font, mic, light, warning
-    
+
     # print(light, mic)
 
     # 把畫面清空
